36f902442a0d1bb4628b258c96b03fbd50deaca8
alex <me@fomin3.ru>
2022-09-28 11:44:27 +0300

    accessor and config added

---
+++ aiohttp_server/aiohttp_server/app/forum/views.py
@@ -0,0 +1,5 @@
+import aiohttp_jinja2
+
+@aiohttp_jinja2.template('index.html')
+async def index(request):
+    return {'title': 'v1 of aiohttp web-server'}
---
+++ aiohttp_server/aiohttp_server/main.py
@@ -0,0 +1,31 @@
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+from app.settings import config, BASE_DIR
+
+# config file
+def setup_config(application):
+    application['config'] = config
+
+# url-routes
+def setup_routes(application):
+    from app.forum.routes import setup_routes as setup_forum_routes
+    setup_forum_routes(application)
+
+# templates dir
+def setup_external_libraries(application: web.Application) -> None:
+    aiohttp_jinja2.setup(application, loader=jinja2.FileSystemLoader(f'{BASE_DIR}/templates'))
+
+# setup of the whole app
+def setup_app(application):
+    setup_config(application)
+    setup_external_libraries(application)
+    setup_routes(application)
+
+# create web-app
+app = web.Application()
+
+if __name__ == '__main__':
+    setup_app(app)
+    web.run_app(app, port=config['common']['port']) # run app
