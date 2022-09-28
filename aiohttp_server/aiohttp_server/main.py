d9be1a8cac43dd12562106d6531c99a8ce2b738c
alex <me@fomin3.ru>
2022-09-28 11:55:17 +0300

    db connect

--- aiohttp_server/aiohttp_server/main.py
+++ aiohttp_server/aiohttp_server/main.py
@@ -2,6 +2,7 @@ import aiohttp_jinja2
 import jinja2
 from aiohttp import web
 
+from app.store.database.accessor import PostgresAccessor
 from app.settings import config, BASE_DIR
 
 # config file
@@ -17,9 +18,15 @@ def setup_routes(application):
 def setup_external_libraries(application: web.Application) -> None:
     aiohttp_jinja2.setup(application, loader=jinja2.FileSystemLoader(f'{BASE_DIR}/templates'))
 
+# db connect to app
+def setup_accessors(application):
+    application['db'] = PostgresAccessor()
+    application['db'].setup(application)
+
 # setup of the whole app
 def setup_app(application):
     setup_config(application)
+    setup_accessors(application)
     setup_external_libraries(application)
     setup_routes(application)
 
